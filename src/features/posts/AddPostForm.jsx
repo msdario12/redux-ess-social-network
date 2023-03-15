03e955800dfc36d99585b93bdff64f4d4bad7dc0
msdario12 <mansilladario94@gmail.com>
2023-03-15 12:14:10 -0300

    In addPostForm, added a dispatch thunk for create a new post, manage the status of the post.

--- src/features/posts/AddPostForm.jsx
+++ src/features/posts/AddPostForm.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react"
 import { useDispatch, useSelector } from "react-redux";
-import { postAdded } from "./postsSlice";
+import { addNewPost, postAdded } from "./postsSlice";
 
 
 export const AddPostForm = () => {
     const [title, setTitle] = useState();
     const [content, setContent] = useState();
     const [userId, setUserId] = useState();
+    const [addRequestStatus, setAddRequestStatus] = useState('idle');
 
     const dispatch = useDispatch();
 
@@ -14,21 +15,34 @@ export const AddPostForm = () => {
 
     const onTitleChanged = e => setTitle(e.target.value)
     const onContentChanged = e => setContent(e.target.value);
-    const onAuthorChanged = e => setUserId(e.target.value);
+    const onAuthorChanged = e => {
+        // const name = e.target.value
+        // const user = users.find(user => user.name === name)
+        // if (user) {setUserId(user.id)}
+        setUserId(e.target.value)
+    };
 
+    const canSave = 
+        [title, content, userId].every(Boolean) && addRequestStatus === 'idle'
 
-    const onSavePostClicked = () => {
-        if (content&&title) {
-            dispatch(postAdded(title, content, userId));
-            setTitle('');
-            setContent('');
+    const onSavePostClicked = async () => {
+        if (canSave) {
+            try {
+                setAddRequestStatus('pending')
+                await dispatch(addNewPost({title, content, user: userId})).unwrap()
+                setTitle('')
+                setContent('')
+                setUserId('')
+            } catch (error) {
+                console.error('Failed to save the post: ', error)
+            } finally {
+                setAddRequestStatus('idle')
+            }
         }
-    }
-
-    const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
-
+        }
+    
     const userOptions = users.map(user => (
-        <option key={user.id} value={userId}>
+        <option key={user.id} value={user.id}>
             {user.name}
         </option>
     ))
