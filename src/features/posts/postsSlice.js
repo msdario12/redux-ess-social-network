e84cca0a5ecf310227079a1a50a720c7e4336c33
msdario12 <mansilladario94@gmail.com>
2023-03-15 10:11:39 -0300

    Change initial state in postSlice

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -2,40 +2,11 @@ import { sub } from 'date-fns'
 
 const { createSlice, nanoid } = require('@reduxjs/toolkit')
 
-const initialState = [
-  {
-    id: '1',
-    title: 'First Post',
-    content: 'Hello!',
-    user: 'Pedro',
-    date: sub(new Date(), { minutes: 10 }).toISOString(),
-    reactions: {thumbsUp: 4, hooray: 0, heart: 2, rocket: 0, eyes: 0},
-  },
-  {
-    id: '2',
-    title: '2First Post',
-    content: '2Hello!',
-    user: 'Pedro1',
-    date: sub(new Date(), { minutes: 15 }).toISOString(),
-    reactions: {thumbsUp: 12, hooray: 0, heart: 52, rocket: 0, eyes: 0},
-  },
-  {
-    id: '3',
-    title: '3First Post',
-    content: '3Hello!',
-    user: 'Pedro2',
-    date: sub(new Date(), { minutes: 25 }).toISOString(),
-    reactions: {thumbsUp: 743, hooray: 0, heart: 0, rocket: 345, eyes: 0},
-  },
-  {
-    id: '4',
-    title: '4First Post',
-    content: '4Hello!',
-    user: 'Pedro3',
-    date: sub(new Date(), { minutes: 35 }).toISOString(),
-    reactions: {thumbsUp: 0, hooray: 2353, heart: 25, rocket: 0, eyes: 0},
-  },
-]
+const initialState = {
+  posts: [],
+  status: 'idle',
+  erro: null,
+}
 
 const postsSlice = createSlice({
   name: 'posts',
