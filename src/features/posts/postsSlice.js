5166deb498dbbc485ababeb36d9c5d98bff43989
msdario12 <mansilladario94@gmail.com>
2023-03-15 10:35:59 -0300

    Change postsSlice, posts reference to new structure of data.

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -14,7 +14,7 @@ const postsSlice = createSlice({
   reducers: {
     postAdded: {
       reducer: (state, action) => {
-        state.push(action.payload)
+        state.posts.push(action.payload)
       },
       prepare: (title, content, userId) => {
         return {
@@ -24,43 +24,42 @@ const postsSlice = createSlice({
             content,
             user: userId,
             date: new Date().toISOString(),
-            reactions: {thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0}
+            reactions: { thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0 },
           },
         }
       },
     },
     postUpdated: (state, action) => {
       const { id, title, content } = action.payload
-      const postToEdit = state.find((post) => post.id === id)
+      const postToEdit = state.posts.find((post) => post.id === id)
       if (postToEdit) {
         postToEdit.title = title
         postToEdit.content = content
       }
     },
     reactionAdded: {
-        reducer: (state, action) => {
-            const {id, reaction} = action.payload
-            const postToAddReaction = state.find((post) => post.id === id);
-            if (postToAddReaction){
-                postToAddReaction.reactions[reaction]++
-            }
-            },
-        prepare: (id, reaction) => {
-            return {
-                payload: {
-                    id: id,
-                    reaction: reaction
-                }
-            }
+      reducer: (state, action) => {
+        const { id, reaction } = action.payload
+        const postToAddReaction = state.posts.find((post) => post.id === id)
+        if (postToAddReaction) {
+          postToAddReaction.reactions[reaction]++
         }
-    }
+      },
+      prepare: (id, reaction) => {
+        return {
+          payload: {
+            id: id,
+            reaction: reaction,
+          },
+        }
+      },
+    },
   },
 })
 
 export default postsSlice.reducer
-export const { postAdded } = postsSlice.actions
-export const { postUpdated } = postsSlice.actions
-export const { reactionAdded } = postsSlice.actions
+export const { postAdded, postUpdated, reactionAdded } = postsSlice.actions
 
-export const selectAllPosts = state => state.posts
-export const selectPostById = (state, postId) => state.posts.find(post=>post.id === postId)
+export const selectAllPosts = (state) => state.posts.posts
+export const selectPostById = (state, postId) =>
+  state.posts.posts.find((post) => post.id === postId)
