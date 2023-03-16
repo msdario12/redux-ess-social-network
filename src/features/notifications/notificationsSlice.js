9013d3567d9de0bb8d862abf9924c32ca2687c27
msdario12 <mansilladario94@gmail.com>
2023-03-16 00:20:00 -0300

    Create notificationSlice, with the thunk "fetchNotifications"

---
+++ src/features/notifications/notificationsSlice.js
@@ -0,0 +1,31 @@
+import { client } from '../../api/client'
+const { createSlice, createAsyncThunk } = require('@reduxjs/toolkit')
+
+const fetchNotifications = createAsyncThunk(
+  'notifications/fetchNotifications',
+  async (_, { getState }) => {
+    const allNotifications = selectAllNotifications(getState())
+    const [latestNotification] = allNotifications
+    const latestTimestamp = latestNotification ? latestNotification.date : ''
+    const response = await client.get(
+      `fakeApi/notifications?since=${latestTimestamp}`
+    )
+    return response.data
+  }
+)
+
+const notificationsSlice = createSlice({
+  name: 'notifications',
+  initialState: [],
+  reducer: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchNotifications.fulfilled, (state, action) => {
+      state.push(...action.payload)
+      // Sort with newest first
+      state.sort((a, b) => b.date.localeCompare(a.date))
+    })
+  },
+})
+
+export default notificationsSlice.reducer
+export const selectAllNotifications = (state) => state.notifications
